812a0f0bf345d7f527fd9087d766fbcdbafe5c80
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-05 19:58:43 +0000

    adding differnent name to package

--- source/cmd/start.go
+++ source/cmd/start.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 
-	"masters3d.com/keyboard_layout_config_mapper/source/keyboard_config"
+	keycode "masters3d.com/keyboard_layout_config_mapper/source/keyboard_config"
 )
 
 func main() {
-	fmt.Printf(fmt.Sprint(len(keyboard_config.FullKeyboard)))
+	fmt.Printf(fmt.Sprint(len(keycode.FullKeyboard)))
 
-	for index, element := range keyboard_config.FullKeyboard {
+	for index, element := range keycode.FullKeyboard {
 		fmt.Println(index, element)
 	}
+
+	fmt.Println(keycode.KC_A)
+	fmt.Println(keycode.KC_F9)
+
 }
--- source/keyboard_config/readme.md
+++ source/keyboard_config/readme.md
@@ -1,6 +1,6 @@
 
 
-intal the following to update the string represenation of consts 
+install the following to update the string represenation of consts 
 `go install golang.org/x/tools/cmd/stringer`
 
 then run the below to update the auto generated source code
