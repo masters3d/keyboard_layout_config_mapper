d17fd413003106ac408410644347172e3e2832ef
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-15 15:41:22 -0700

    replace some magical values

--- source/keyboard_config/keyboardconfig.go
+++ source/keyboard_config/keyboardconfig.go
@@ -58,12 +58,12 @@ func convertLayerToErgodoxPrexy(input KeycodeLayerFull) []KeyCodeRepresentable {
 	var collectedArray = []KeyCodeRepresentable{}
 
 	indexesToIgnore := IntSlice{
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, //function layer since Ergodox does not have a function layer
-		6 + (14 * 3), 7 + (14 * 3), // g h row. surprisingly the ErgodoxPrexy does not map anything on the middle here but intead it does to the next row
-		5 + (14 * 5), 6 + (14 * 5), 7 + (14 * 5), 8 + (14 * 5), // arrow row. We need to skip two values here
+		6 + (columnFullCount * 3), 7 + (columnFullCount * 3), // g h row. surprisingly the ErgodoxPrexy does not map anything on the middle here but intead it does to the next row
+		5 + (columnFullCount * 5), 6 + (columnFullCount * 5), 7 + (columnFullCount * 5), 8 + (columnFullCount * 5), // arrow row. We need to skip two values here
 	}
-	lastRowCount := 1 //we will skipping these many rows from the end
-	for indexRow := 0; indexRow < rowCount-lastRowCount; indexRow++ {
+	firstRowCount := 1 // we will be starting at this row index. firstfunction layer is skipped since Ergodox does not have a function layer
+	lastRowCount := 1  //we will skipping these many rows from the end
+	for indexRow := firstRowCount; indexRow < rowCount-lastRowCount; indexRow++ {
 
 		for indexColumn := 0; indexColumn < columnFullCount; indexColumn++ {
 			currentIndex := indexColumn + (indexRow * columnFullCount)
@@ -75,12 +75,12 @@ func convertLayerToErgodoxPrexy(input KeycodeLayerFull) []KeyCodeRepresentable {
 		}
 	}
 	indexesForThumbCluster := IntSlice{
-		5 + (14 * 5), 5 + (14 * 6), // left side first two keys on thumb
-		8 + (14 * 6), 8 + (14 * 5), // right side first two keys on thumb; The order here is swapped
-		4 + (14 * 6),                             // left side second row single key
-		9 + (14 * 6),                             // right side second row single key
-		1 + (14 * 6), 2 + (14 * 6), 3 + (14 * 6), //left side bottom row single key
-		10 + (14 * 6), 11 + (14 * 6), 12 + (14 * 6), //right side bottom row single key
+		5 + (columnFullCount * 5), 5 + (columnFullCount * 6), // left side first two keys on thumb
+		8 + (columnFullCount * 6), 8 + (columnFullCount * 5), // right side first two keys on thumb; The order here is swapped
+		4 + (columnFullCount * 6),                                                       // left side second row single key
+		9 + (columnFullCount * 6),                                                       // right side second row single key
+		1 + (columnFullCount * 6), 2 + (columnFullCount * 6), 3 + (columnFullCount * 6), //left side bottom row single key
+		10 + (columnFullCount * 6), 11 + (columnFullCount * 6), 12 + (columnFullCount * 6), //right side bottom row single key
 	}
 
 	for _, indexAsValue := range indexesForThumbCluster {
