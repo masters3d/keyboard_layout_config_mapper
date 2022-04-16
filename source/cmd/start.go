85c039777badce3414e9ce3a434cf34f74f1f5b3
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-16 12:56:32 -0700

    added validation to tests

--- source/cmd/start.go
+++ source/cmd/start.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	keycode "masters3d.com/keyboard_layout_config_mapper/source/keyboard_config"
 )
 
 func main() {
-	keycode.MergeTest()
+	fmt.Println(keycode.KC_TRANSPARENT)
 }
--- source/keyboard_config/keyboardconfig.go
+++ source/keyboard_config/keyboardconfig.go
@@ -1,7 +1,5 @@
 package keyboard_config
 
-import "fmt"
-
 const rowCount = 7
 const columnHalfCount = 7
 const columnFullCount = columnHalfCount * 2
@@ -10,37 +8,6 @@ const fullKeyboardKeyCount = halfKeyboardKeyCount * 2
 
 type KeycodeLayerHalf = [halfKeyboardKeyCount]KeyCodeRepresentable
 type KeycodeLayerFull = [fullKeyboardKeyCount]KeyCodeRepresentable
-
-var mainLayerLeft = KeycodeLayerHalf{
-	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, // function keys
-	ST_MACRO_Screenshot, LSFT(KC_1), KC_GRAVE, KC_MINUS, KC_EQUAL, KC_SLASH, KC_TRANSPARENT, // number keys
-	KC_TRANSPARENT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_TRANSPARENT,
-	KC_ESCAPE, KC_A, KC_S, KC_D, KC_F, KC_G, KC_TRANSPARENT,
-	KC_TRANSPARENT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRANSPARENT,
-	KC_TRANSPARENT, TO(0), TO(1), TO(2), TO(3), MO(1), KC_TRANSPARENT,
-	KC_TRANSPARENT, KC_LEFT_SHIFT, KC_LEFT_GUI, KC_LEFT_ALT, KC_LEFT_CTRL, KC_RIGHT_GUI, KC_TRANSPARENT,
-}
-var mainLayerRight = KeycodeLayerHalf{
-	KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, // function keys
-	KC_TRANSPARENT, KC_BACKSLASH, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_TRANSPARENT, // number keys
-	KC_TRANSPARENT, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_TRANSPARENT,
-	KC_TRANSPARENT, KC_H, KC_J, KC_K, KC_L, KC_DOT, KC_ENTER,
-	KC_TRANSPARENT, KC_N, KC_M, KC_SEMICOLON, KC_QUOTE, KC_COMMA, KC_TRANSPARENT,
-	KC_TRANSPARENT, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRANSPARENT,
-	KC_TRANSPARENT, KC_ESCAPE, KC_DELETE, KC_TAB, KC_BACKSPACE, KC_SPACE, KC_TRANSPARENT,
-}
-
-func MergeTest() {
-	fmt.Println("#####        start        #####")
-	for index, value := range mergeHalfs(mainLayerLeft, mainLayerRight) {
-		if (index)%rowCount == 0 && index != 0 {
-			fmt.Println("")
-		}
-		fmt.Print(value, ", ")
-	}
-	fmt.Println("")
-	fmt.Println("#####        end        #####")
-}
 
 type IntSlice []int
 
