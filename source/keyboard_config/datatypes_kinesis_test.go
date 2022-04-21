228c830bb6d8c3ec89668ac0b0c1c4c391dfb302
Chéyo Jiménez <masters3d@users.noreply.github.com>
2022-04-20 20:37:41 -0700

    adding failing test

--- source/keyboard_config/datatypes_kinesis_test.go
+++ source/keyboard_config/datatypes_kinesis_test.go
@@ -14,14 +14,32 @@ func Test_Creating_KeyPad_Descriptions(t *testing.T) {
 	}
 
 }
+func Test_Creating_KeyPad_Descriptions_FullArray(t *testing.T) {
+
+	var fullLayerTarget = mergeHalfs(mainLayerLeft, mainLayerRight)
+	var fullLayerTargetAdv2Default = mergeHalfs(Adv2TopLayerLeft, Adv2TopLayerRight)
+
+	for index, keycode_each_target := range fullLayerTarget {
+		if keycode_each_target.String() == "KC_TRANSPARENT" {
+			continue // we don't have mapping for transparent
+		}
+
+		var keycode_each_source_default = fullLayerTargetAdv2Default[index]
+
+		if keycode_each_target != keycode_each_source_default {
+			var message = ":`" + keycode_each_target.String() + "` needs mapping `" + keycode_each_source_default.String() + "`"
+			t.Error(message)
+		}
+
+	}
+
+}
 
 func Test_Creating_KeyPad_DescriptionsArray(t *testing.T) {
 
-	for index, keycode_each := range Adv2TopLayerLeft {
+	for _, keycode_each := range Adv2TopLayerLeft {
 
-		println(keycode_each, index)
 		keycode_each_asString := keycode_each.String()
-		println(keycode_each_asString)
 
 		if keycode_each_asString == "KC_TRANSPARENT" {
 			continue // we don't have mapping for transparent
